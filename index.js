a859dd245ac48d733c5f478d78cfdac275d00153
Daniel Etter <etter.dt@gmail.com>
2018-02-26 21:41:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ function add(a, b) {
   number = a + b;
   return number;
 }
+
+function subtract(a, b) {
+  number = a - b;
+  return number;
+}
+
+function multiply(a, b) {
+  number = a * b;
+  return number;
+}
+
+function divide(a, b) {
+  number = a / b;
+  return number;
+}
